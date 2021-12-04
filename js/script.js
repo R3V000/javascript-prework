97cfe5f0e1bdc6364dfb0ec1a120259ee139162e
Dominik Stencel <dominik.stencel12@gmail.com>
2021-12-04 22:45:10 +0100

    Added playgame function also added buttons in html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+function playGame(playerInput){
+clearMessages();
+
 function getMoveName(argMoveId){
     if(argMoveId == 1){
       return 'kamień';
@@ -36,11 +39,15 @@ let computerMove = getMoveName(randomNumber);
 printMessage('Mój ruch to: ' + computerMove);
 
 
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Gracz wpisał: ' + playerInput);
 let playerMove = getMoveName(playerInput);
 
 
 
 displayResult(computerMove, playerMove);
+}
+
+
+document.getElementById('play-rock').addEventListener('click',function(){playGame(1)});
+document.getElementById('play-paper').addEventListener('click',function(){playGame(2)});
+document.getElementById('play-scissors').addEventListener('click',function(){playGame(3)});
